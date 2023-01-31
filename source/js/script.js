bf147735923adfff27d0c29b6989a5389252c51c
B3rzxd3v <d3vilc0re@gmail.com>
2023-01-31 18:02:27 +0700

    Add Intro section

--- source/js/script.js
+++ source/js/script.js
@@ -1,160 +1,75 @@
 const burger = document.querySelector('.header__button');
 const header = document.querySelector('.header');
+const inputs = document.querySelectorAll('input[type="tel"');
+
+const PHONE_SCHEME = '+7-___-___-__-__';
+let currentPos = 3;
+let currentValue = '';
 
 burger.addEventListener('click', () => {
   header.classList.toggle('header--opened');
 });
-jQuery(document).ready(function($){
-	//set animation timing
-	var animationDelay = 2500,
-		//loading bar effect
-		barAnimationDelay = 3800,
-		barWaiting = barAnimationDelay - 3000, //3000 is the duration of the transition on the loading bar - set in the scss/css file
-		//letters effect
-		lettersDelay = 50,
-		//type effect
-		typeLettersDelay = 150,
-		selectionDuration = 500,
-		typeAnimationDelay = selectionDuration + 800,
-		//clip effect
-		revealDuration = 600,
-		revealAnimationDelay = 1500;
 
-	initHeadline();
+inputs.forEach((input) => {
+  input.addEventListener('focus', (e) => {
+    if (e.target.value.length === 0) {
+      e.target.value = PHONE_SCHEME;
+      setTimeout(() => {
+        e.target.selectionStart = 3;
+        e.target.selectionEnd = 3;
+      }, 1);
+    }
+    currentValue = e.target.value;
+  });
 
+  input.addEventListener('click', (e) => {
+    if (e.target.value === PHONE_SCHEME) return;
+    currentPos = getCursorPosition(e.target);
+  });
 
-	function initHeadline() {
-		//insert <i> element for each letter of a changing word
-		singleLetters($('.cd-headline.letters').find('b'));
-		//initialise headline animation
-		animateHeadline($('.cd-headline'));
-	}
+  input.addEventListener('input', (e) => {
+    currentPos = e.target.value === PHONE_SCHEME
+      ? 3 : getCursorPosition(e.target) - 1;
+    const letter = e.target.value[currentPos];
+    if (e.target.value.length < currentValue.length) {
+      currentPos++;
+      currentPos < 3 ? currentPos = 3: null;
+      if (currentValue[currentPos] === '-') currentPos--;
+      currentValue = (currentValue.slice(0, currentPos) + '_' + currentValue.slice(currentPos + 1)).slice(0, 16);
+      e.target.value = currentValue;
+      setTimeout(() => {
+        e.target.selectionStart = currentPos;
+        e.target.selectionEnd = currentPos;
+      }, 1);
+      return;
+    }
+    if (currentValue[currentPos] === '-') currentPos++;
+    currentValue = (currentValue.slice(0, currentPos) + letter + currentValue.slice(currentPos + 1)).slice(0, 16);
+    currentPos === 16 ? null : currentPos++;
+    setTimeout(() => {
+      e.target.selectionStart = currentPos;
+      e.target.selectionEnd = currentPos;
+    }, 1);
+    e.target.value = currentValue;
+  });
 
-	function singleLetters($words) {
-		$words.each(function(){
-			var word = $(this),
-				letters = word.text().split(''),
-				selected = word.hasClass('is-visible');
-			for (i in letters) {
-				if(word.parents('.rotate-2').length > 0) letters[i] = '<em>' + letters[i] + '</em>';
-				letters[i] = (selected) ? '<i class="in">' + letters[i] + '</i>': '<i>' + letters[i] + '</i>';
-			}
-		    var newLetters = letters.join('');
-		    word.html(newLetters).css('opacity', 1);
-		});
-	}
-
-	function animateHeadline($headlines) {
-		var duration = animationDelay;
-		$headlines.each(function(){
-			var headline = $(this);
-
-			if(headline.hasClass('loading-bar')) {
-				duration = barAnimationDelay;
-				setTimeout(function(){ headline.find('.cd-words-wrapper').addClass('is-loading') }, barWaiting);
-			} else if (headline.hasClass('clip')){
-				var spanWrapper = headline.find('.cd-words-wrapper'),
-					newWidth = spanWrapper.width() + 10
-				spanWrapper.css('width', newWidth);
-			} else if (!headline.hasClass('type') ) {
-				//assign to .cd-words-wrapper the width of its longest word
-				var words = headline.find('.cd-words-wrapper b'),
-					width = 0;
-				words.each(function(){
-					var wordWidth = $(this).width();
-				    if (wordWidth > width) width = wordWidth;
-				});
-				headline.find('.cd-words-wrapper').css('width', width);
-			};
-
-			//trigger animation
-			setTimeout(function(){ hideWord( headline.find('.is-visible').eq(0) ) }, duration);
-		});
-	}
-
-	function hideWord($word) {
-		var nextWord = takeNext($word);
-
-		if($word.parents('.cd-headline').hasClass('type')) {
-			var parentSpan = $word.parent('.cd-words-wrapper');
-			parentSpan.addClass('selected').removeClass('waiting');
-			setTimeout(function(){
-				parentSpan.removeClass('selected');
-				$word.removeClass('is-visible').addClass('is-hidden').children('i').removeClass('in').addClass('out');
-			}, selectionDuration);
-			setTimeout(function(){ showWord(nextWord, typeLettersDelay) }, typeAnimationDelay);
-
-		} else if($word.parents('.cd-headline').hasClass('letters')) {
-			var bool = ($word.children('i').length >= nextWord.children('i').length) ? true : false;
-			hideLetter($word.find('i').eq(0), $word, bool, lettersDelay);
-			showLetter(nextWord.find('i').eq(0), nextWord, bool, lettersDelay);
-
-		}  else if($word.parents('.cd-headline').hasClass('clip')) {
-			$word.parents('.cd-words-wrapper').animate({ width : '2px' }, revealDuration, function(){
-				switchWord($word, nextWord);
-				showWord(nextWord);
-			});
-
-		} else if ($word.parents('.cd-headline').hasClass('loading-bar')){
-			$word.parents('.cd-words-wrapper').removeClass('is-loading');
-			switchWord($word, nextWord);
-			setTimeout(function(){ hideWord(nextWord) }, barAnimationDelay);
-			setTimeout(function(){ $word.parents('.cd-words-wrapper').addClass('is-loading') }, barWaiting);
-
-		} else {
-			switchWord($word, nextWord);
-			setTimeout(function(){ hideWord(nextWord) }, animationDelay);
-		}
-	}
-
-	function showWord($word, $duration) {
-		if($word.parents('.cd-headline').hasClass('type')) {
-			showLetter($word.find('i').eq(0), $word, false, $duration);
-			$word.addClass('is-visible').removeClass('is-hidden');
-
-		}  else if($word.parents('.cd-headline').hasClass('clip')) {
-			$word.parents('.cd-words-wrapper').animate({ 'width' : $word.width() + 10 }, revealDuration, function(){
-				setTimeout(function(){ hideWord($word) }, revealAnimationDelay);
-			});
-		}
-	}
-
-	function hideLetter($letter, $word, $bool, $duration) {
-		$letter.removeClass('in').addClass('out');
-
-		if(!$letter.is(':last-child')) {
-		 	setTimeout(function(){ hideLetter($letter.next(), $word, $bool, $duration); }, $duration);
-		} else if($bool) {
-		 	setTimeout(function(){ hideWord(takeNext($word)) }, animationDelay);
-		}
-
-		if($letter.is(':last-child') && $('html').hasClass('no-csstransitions')) {
-			var nextWord = takeNext($word);
-			switchWord($word, nextWord);
-		}
-	}
-
-	function showLetter($letter, $word, $bool, $duration) {
-		$letter.addClass('in').removeClass('out');
-
-		if(!$letter.is(':last-child')) {
-			setTimeout(function(){ showLetter($letter.next(), $word, $bool, $duration); }, $duration);
-		} else {
-			if($word.parents('.cd-headline').hasClass('type')) { setTimeout(function(){ $word.parents('.cd-words-wrapper').addClass('waiting'); }, 200);}
-			if(!$bool) { setTimeout(function(){ hideWord($word) }, animationDelay) }
-		}
-	}
-
-	function takeNext($word) {
-		return (!$word.is(':last-child')) ? $word.next() : $word.parent().children().eq(0);
-	}
-
-	function takePrev($word) {
-		return (!$word.is(':first-child')) ? $word.prev() : $word.parent().children().last();
-	}
-
-	function switchWord($oldWord, $newWord) {
-		$oldWord.removeClass('is-visible').addClass('is-hidden');
-		$newWord.removeClass('is-hidden').addClass('is-visible');
-	}
+  input.addEventListener('blur', (e) => {
+    e.target.value = e.target.value === PHONE_SCHEME
+      ? ''
+      : e.target.value;
+  });
 });
+
+function getCursorPosition(ctrl) {
+  let CaretPos = 0;
+  if (document.selection) {
+    ctrl.focus();
+    const Sel = document.selection.createRange();
+    Sel.moveStart('character', -ctrl.value.length);
+    CaretPos = Sel.text.length;
+  } else if (ctrl.selectionStart || ctrl.selectionStart == '0') {
+    CaretPos = ctrl.selectionStart;
+  }
+  return CaretPos;
+}
+
