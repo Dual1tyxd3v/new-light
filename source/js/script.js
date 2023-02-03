6ef543fa682597b8318928cf5a75682dd3c26cf8
B3rzxd3v <d3vilc0re@gmail.com>
2023-02-03 10:16:55 +0700

    Add slider

--- source/js/script.js
+++ source/js/script.js
@@ -3,6 +3,14 @@ const header = document.querySelector('.header');
 const inputs = document.querySelectorAll('input[type="tel"');
 const portfolioMenuBtns = document.querySelectorAll('.portfolio__menu-item');
 const portfolioContainer = document.querySelector('.portfolio__container');
+const portfolioCards = document.querySelectorAll('.portfolio__link');
+const popup = document.querySelector('.popup');
+const popupClose = document.querySelector('.popup__btn');
+const sliderRightBtn = document.querySelector('.popup__control--right');
+const sliderleftBtn = document.querySelector('.popup__control--left');
+const sliderCounter = document.querySelector('.popup__counter');
+const sliderTitle = document.querySelector('.popup__title');
+const sliderWrapper = document.querySelector('.popup__content-wrapper');
 
 const PHONE_SCHEME = '+7-___-___-__-__';
 let currentPos = 3;
@@ -106,7 +114,7 @@ portfolioMenuBtns.forEach((btn) => {
     });
     e.target.classList.add('portfolio__menu-item--active');
     const index = e.target.dataset.index;
-    document.querySelectorAll('.portfolio__link').forEach((link) => {
+    portfolioCards.forEach((link) => {
       if (index === link.dataset.index) {
         link.parentElement.classList.remove('portfolio__item--hidden');
         link.parentElement.classList.add('animated');
@@ -126,4 +134,101 @@ portfolioMenuBtns.forEach((btn) => {
     });
   });
 });
+//
+// слайдер
+// --кнопка закрытия
+popupClose.addEventListener('click', (e) => {
+  popup.classList.toggle('hide');
+});
+// --первая загрузка слайдера
+portfolioCards.forEach((card, i) => {
+  card.addEventListener('click', (e) => loadSlide(e, i));
+});
+
+let currentIndex = 0;
+
+function loadSlide(e, i) {
+  const currentImgSrc = e.target.style.backgroundImage.replace('url("', '').replace('")', '');
+  const prevImgSrc = portfolioCards[i === 0
+    ? portfolioCards.length - 1
+    : i - 1].style.backgroundImage.replace('url("', '').replace('")', '');
+  const nextImgSrc = portfolioCards[i === portfolioCards.length - 1
+    ? 0
+    : i + 1].style.backgroundImage.replace('url("', '').replace('")', '');
+  const currentImg = document.createElement('img');
+  const prevImg = document.createElement('img');
+  const nextImg = document.createElement('img');
+  currentImg.classList.add('img-curr');
+  currentImg.src = currentImgSrc;
+  prevImg.src = prevImgSrc;
+  prevImg.classList.add('img-prev');
+  nextImg.src = nextImgSrc;
+  nextImg.classList.add('img-next');
+  sliderWrapper.innerHTML = '';
+  // currentImg.addEventListener('click', (e) => e.target.classList.toggle('img--scale'));
+  sliderWrapper.append(prevImg, currentImg, nextImg);
+  currentImg.title = e.target.dataset.alt;
+  sliderTitle.innerHTML = e.target.dataset.alt;
+  popup.classList.toggle('hide');
+  currentIndex = i;
+  sliderCounter.innerHTML = `${currentIndex + 1} / ${portfolioCards.length}`;
+}
+// --кнопки смены слайдов
+sliderRightBtn.addEventListener('click', () => changeSlide('right'));
+sliderleftBtn.addEventListener('click', () => changeSlide('left'));
+
+function changeSlide(side) {
+  const prevImg = document.querySelector('.img-prev');
+  const currImg = document.querySelector('.img-curr');
+  const nextImg = document.querySelector('.img-next');
+  let options = {};
+  switch (side) {
+    case "right": options = {
+      currentClass: 'img-prev',
+      toDelete: prevImg,
+      toChange: nextImg,
+      curInd: 'plus',
+      newClass: 'img-next',
+      addAction: 'append'
+    };
+      break;
+    case 'left': options = {
+      currentClass: 'img-next',
+      toDelete: nextImg,
+      toChange: prevImg,
+      curInd: 'minus',
+      newClass: 'img-prev',
+      addAction: 'prepend'
+    };
+      break;
+    default: null;
+      break;
+  }
+  currImg.classList.remove('img-curr');
+  currImg.classList.add(options.currentClass);
+  options.toChange.classList.remove(options.newClass);
+  options.toChange.classList.add('img-curr');
+  prevImg.parentNode.removeChild(options.toDelete);
+  currentIndex = checkIndex(currentIndex, options.curInd);
+  const newImg = document.createElement('img');
+  newImg.classList.add(options.newClass);
+  newImg.src = portfolioCards[checkIndex(currentIndex, options.curInd)].style.backgroundImage.replace('url("', '').replace('")', '');
+  sliderTitle.innerHTML = portfolioCards[currentIndex].dataset.alt;
+  sliderWrapper.classList.remove('popup__content-wrapper--scale');
+  popup.querySelector('.popup__content-wrapper')[options.addAction](newImg);
+  sliderCounter.innerHTML = `${currentIndex + 1} / ${portfolioCards.length}`;
+}
+// --проверка индекса слайда
+function checkIndex(index, action) {
+  if (action === 'plus') {
+    index = index === (portfolioCards.length - 1) ? 0 : index + 1;
+  }
+  if (action === 'minus') {
+    index = index === 0 ? portfolioCards.length - 1 : index - 1;
+  }
+  return index;
+}
+sliderWrapper.addEventListener('click', (e) => {
+  sliderWrapper.classList.toggle('popup__content-wrapper--scale');
+})
 //
